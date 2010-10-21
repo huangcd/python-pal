2b86ae273e6bb60f080573e8277c39016e5420bb
huangcd.thu <huangcd.thu@03a512d0-f10a-5dfa-d987-fddcef8ad7e4>
2010-10-21 08:54:13 +0000

    NULL

--- trunk/src/setup.py
+++ trunk/src/setup.py
@@ -1,4 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+Created on 2010-10-21
+@author: huangcd.thu@gmail.com
+'''
+
 from struct import unpack, pack
+"""
+处理SETUP.DAT文件中的配置信息
+"""
 class Setup:
     def decode(self):
         with open('SETUP.DAT', 'r') as f:
@@ -10,7 +21,7 @@ class Setup:
             = unpack(r'H' * 7 + 'H' * 3, content)
 
     def encode(self):
-        with open('SETUP.DAT.bak', 'w') as f:
+        with open('SETUP.DAT', 'w') as f:
             content = pack(r'H' * 7 + 'H' * 3,                         \
             self.key_leftup,    self.key_rightup,  self.key_rightdown, \
             self.key_leftdown,  self.music,        self.sound,         \
