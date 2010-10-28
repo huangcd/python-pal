d271335213836d369e129262e165d6f1b41f8404
huangcd.thu <huangcd.thu@03a512d0-f10a-5dfa-d987-fddcef8ad7e4>
2010-10-28 04:52:46 +0000

    NULL

--- trunk/src/setup.py
+++ trunk/src/setup.py
@@ -7,24 +7,46 @@ Created on 2010-10-21
 '''
 
 from struct import unpack, pack
-"""
-处理SETUP.DAT文件中的配置信息
-"""
+
 class Setup:
+    """
+    处理SETUP.DAT文件中的配置信息
+    """
+    def __init__(self):
+        '''
+        init
+        '''
+        self.key_leftup      = None
+        self.key_rightup     = None
+        self.key_rightdown   = None
+        self.key_leftdown    = None
+        self.music           = None
+        self.sound           = None
+        self.irq             = None
+        self.io_port         = None
+        self.midi            = None
+        self.use_files_on_cd = None
+
     def decode(self):
-        with open('SETUP.DAT', 'r') as f:
-            content = f.read()
+        '''
+        decode information from file SETUP.DAT
+        '''
+        with open('SETUP.DAT', 'r') as handle:
+            content = handle.read()
             self.key_leftup,    self.key_rightup,  self.key_rightdown, \
             self.key_leftdown,  self.music,        self.sound,         \
-            self.IRQ,           self.IOPort,       self.MIDI,          \
-            self.use_files_on_CD                                       \
+            self.irq,           self.io_port,      self.midi,          \
+            self.use_files_on_cd                                       \
             = unpack(r'H' * 7 + 'H' * 3, content)
 
     def encode(self):
-        with open('SETUP.DAT', 'w') as f:
+        '''
+        encode information to file SETUP.DAT
+        '''
+        with open('SETUP.DAT', 'w') as handle:
             content = pack(r'H' * 7 + 'H' * 3,                         \
             self.key_leftup,    self.key_rightup,  self.key_rightdown, \
             self.key_leftdown,  self.music,        self.sound,         \
-            self.IRQ,           self.IOPort,       self.MIDI,          \
-            self.use_files_on_CD                                       )
-            f.write(content)
+            self.irq,           self.io_port,      self.midi,          \
+            self.use_files_on_cd                                       )
+            handle.write(content)
